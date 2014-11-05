ab0cac9fca4ea1da2bee631e69f98e40251d1907
Laeeth Isharc <laeeth@laeeth.com>
2014-11-05 19:47:42 +0000

    all examples work

--- reqrep.d
+++ reqrep.d
@@ -1,50 +1,73 @@
-import nano;
 import std.stdio;
 import std.conv;
+import std.datetime;
+import core.thread;
+import nano;
+
+string date ()
+{
+  return Clock.currTime.toSimpleString();
+}
 
 enum NODE0="node0";
 enum NODE1="node1";
+enum DATE="DATE";
 
-int node0(string url)
+int node0(string surl)
 {
-  int sock = nn_socket (AF_SP, NN_PULL);
+  char* url=cast(char*)surl;
+  int sz_date = cast(int)DATE.length+1;
+  int sock = nn_socket (AF_SP, NN_REP);
   assert (sock >= 0);
-  assert (nn_bind(sock,cast(char*) url) >= 0);
+  assert (nn_bind (sock, url) >= 0);
   while (1)
     {
-      char* buf = cast(char*)0;
-      int bytes = nn_recv (sock, buf, NN_MSG, 0);
+      char *buf = cast(char*)0;
+      int bytes = nn_recv(sock, &buf, NN_MSG, 0);
       assert (bytes >= 0);
-      writefln("NODE0: RECEIVED %s bytes: \"%s\"\n", bytes, to!string(buf));
+      if (to!string(buf)==DATE)
+      {
+          writefln("NODE0: RECEIVED DATE REQUEST");
+          auto d=date();
+          int sz_d = cast(int)d.length + 1; // '\0' too
+          writefln("NODE0: SENDING DATE %s", d);
+          bytes = nn_send (sock, cast(char*)d, sz_d, 0);
+          assert (bytes == sz_d);
+        }
       nn_freemsg (buf);
     }
-    return 0;
-}
-
-int node1 (string url, string msg)
-{
-  int sz_msg = cast(int)msg.length+1;
-  int sock = nn_socket (AF_SP, NN_PUSH);
-  assert (sock >= 0);
-  assert (nn_connect (sock, cast(char*)url) >= 0);
-  writefln ("NODE1: SENDING \"%s\"\n", cast(char*)msg);
-  int bytes = nn_send (sock, cast(char*)msg, sz_msg, 0);
-  assert (bytes == sz_msg);
   return nn_shutdown (sock, 0);
 }
 
+
+int node1 (string url)
+{
+  int sz_date = cast(int)DATE.length+1;
+  char *buf = cast(char*)0;
+  int bytes = -1;
+  int sock = nn_socket (AF_SP, NN_REQ);
+  assert (sock >= 0);
+  assert (nn_connect (sock, cast(char*)url) >= 0);
+  writefln("NODE1: SENDING DATE REQUEST %s", DATE);
+  bytes = nn_send (sock, cast(char*)DATE, sz_date, 0);
+  assert (bytes == sz_date);
+  bytes = nn_recv (sock, &buf, NN_MSG, 0);
+  assert (bytes >= 0);
+  writefln("NODE1: RECEIVED DATE %s", to!string(buf), bytes);
+  nn_freemsg (buf);
+  return nn_shutdown (sock, 0);
+}
+
+ 
 int main(string[] argv)
 {
   if (argv.length>1)
-    if (NODE0==argv[1])
+    if (argv[1]==NODE0)
       return node0(argv[2]);
-    else if (argv.length>3)
-      if (NODE1==argv[1])
-      return node1(argv[2], argv[3]);
-  else
-    {
-      writefln("Usage: pipeline %s|%s <URL> <ARG> ...'",NODE0, NODE1);
-      return 1;
-    }   
-    return 0;
+  if (argv.length>1)
+      if (argv[1]==NODE1)
+        return node1(argv[2]);
+   writefln("Usage: reqrep %s|%s <URL> <ARG> ...'",NODE0, NODE1);
+   return 1;
 }
+
